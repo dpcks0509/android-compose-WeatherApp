36cfa800ce2e9bcac5274ea367a983762341b65b
dpcks0509 <dpcks0509@gmail.com>
2025-01-18 22:15:30 +0900

    fix: DefaultLocationTracker 오류 수정

--- app/src/main/java/com/plcoding/weatherapp/data/location/DefaultLocationTracker.kt
+++ app/src/main/java/com/plcoding/weatherapp/data/location/DefaultLocationTracker.kt
@@ -9,42 +9,42 @@ import android.location.LocationManager
 import androidx.core.content.ContextCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.plcoding.weatherapp.domain.location.LocationTracker
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.suspendCancellableCoroutine
 import javax.inject.Inject
 import kotlin.coroutines.resume
 
+@ExperimentalCoroutinesApi
 class DefaultLocationTracker @Inject constructor(
     private val locationClient: FusedLocationProviderClient,
     private val application: Application
-) : LocationTracker {
+): LocationTracker {
+
     override suspend fun getCurrentLocation(): Location? {
-        val hasAccessFindLocationPermission = ContextCompat.checkSelfPermission(
+        val hasAccessFineLocationPermission = ContextCompat.checkSelfPermission(
             application,
             Manifest.permission.ACCESS_FINE_LOCATION
         ) == PackageManager.PERMISSION_GRANTED
-
         val hasAccessCoarseLocationPermission = ContextCompat.checkSelfPermission(
             application,
             Manifest.permission.ACCESS_COARSE_LOCATION
         ) == PackageManager.PERMISSION_GRANTED
 
-        val locationManager =
-            application.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        val locationManager = application.getSystemService(Context.LOCATION_SERVICE) as LocationManager
         val isGpsEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER) ||
                 locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
-        if (!hasAccessFindLocationPermission || !hasAccessCoarseLocationPermission || !isGpsEnabled) {
+        if(!hasAccessCoarseLocationPermission || !hasAccessFineLocationPermission || !isGpsEnabled) {
             return null
         }
 
         return suspendCancellableCoroutine { cont ->
             locationClient.lastLocation.apply {
-                if (isComplete) {
-                    if (isSuccessful) {
+                if(isComplete) {
+                    if(isSuccessful) {
                         cont.resume(result)
                     } else {
                         cont.resume(null)
                     }
-                } else {
                     return@suspendCancellableCoroutine
                 }
                 addOnSuccessListener {
